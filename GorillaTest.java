2bb0e12e9cae20ebaf4794246f8c839bad8dca6b
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-12 12:05:45 -0400

    Finished the app.

--- GorillaTest.java
+++ GorillaTest.java
@@ -1,10 +1,27 @@
 class GorillaTest {
     public static void main(String[] args) {
         
-        Mammal mam = new Mammal();
+        // Mammal mam = new Mammal();
         Gorilla gorilla = new Gorilla(100);
 
-        System.out.println(ape);
+        // System.out.println(gorilla);
 
+        gorilla.displayEnergy();
+
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+
+        gorilla.displayEnergy();
+
+        gorilla.eatBananas();
+        gorilla.eatBananas();
+
+        gorilla.displayEnergy();
+
+        gorilla.climb();
+        gorilla.climb();
+
+        gorilla.displayEnergy();
     }
 }
