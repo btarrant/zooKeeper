974fb7f535c6cee7e3a913f162b35689c4d02185
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-12 10:13:48 -0400

    Set up the Gorilla class.

--- Gorilla.java
+++ Gorilla.java
@@ -7,14 +7,20 @@ climb(). */
 public class Gorilla {
 /* For the throwSomething() method, have it print out a message indicating that the
 gorilla has thrown something, as well as decrease the energy level by 5. */
-
+    public void throwSomething() {
+    
+    }   
 
 /* For the eatBananas() method, have it print out a message indicating the gorilla's
 satisfaction and increase its energy by 10. */
+    public void eatBananas() {
 
+    }
 
 /* For the climb() method, have it print out a message indicating the gorilla has
 climbed a tree and decrease its energy by 10. */
+    public void climb() {
 
+    }
 
 }
